11dfa54f9508e8da281e2899ea621a7548fed7a8
tinashejima <jimatinashe@gmail.com>
2024-11-12 16:00:30 +0200

    add code

--- com.inn.cafe/src/main/java/com/inn/cafe/POJO/User.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/POJO/User.java
@@ -1,16 +1,52 @@
 package com.inn.cafe.POJO;
-import lombook.Data;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Data;
 
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
+import java.io.Serializable;
+
+
+@NamedQuery(name = "User.findByEmailId", query = "select u from User u where u.email=:email")
 @Data
 @Entity
 @DynamicUpdate
 @DynamicInsert
 @Table(name = "user")
-public class User {
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+
+    @Column(name = "name")
+    private String name;
+
+
+    @Column(name = "contactNumber")
+    private String contactNumber;
+
+    @Column(name = "email")
+    private String email;
+
+
+    @Column(name = "status")
+    private String status;
+
+
+    @Column(name = "role")
+    private String role;
+
+
+
+
+
+
+
 
 }
--- com.inn.cafe/src/main/java/com/inn/cafe/serviceImpl/UserServiceImpl.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/serviceImpl/UserServiceImpl.java
@@ -1,4 +1,57 @@
 package com.inn.cafe.serviceImpl;
 
-public class UserServiceImpl {
+import com.inn.cafe.POJO.User;
+import com.inn.cafe.constants.CafeConstants;
+import com.inn.cafe.dao.UserDao;
+import com.inn.cafe.service.UserService;
+import com.inn.cafe.utils.CafeUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Objects;
+
+@Autowired
+UserDao userDao;
+
+
+@Slf4j
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+        log.info("Inside signUp {}", requestMap);
+        if (validateSignUpMap(requestMap)){
+            User user = userDao.findByEmailId(requestMap.get("emailId"));
+            if (Objects.isNull(user)){
+                userDao.save()
+            }
+            else {
+                 return CafeUtils.getResponseEntity("Email already exists.",HttpStatus.BAD_REQUEST );
+            }
+
+        }
+        else {
+            return CafeUtils.getResponseEntity(CafeConstants.INVALID_DATA, HttpStatus.BAD_REQUEST);
+        }
+
+        return null;
+
+    }
+
+    private boolean validateSignUpMap(Map<String,String> requestMap){
+
+        if (requestMap.containsKey("name") && requestMap.containsKey("contactNumber")
+        && requestMap.containsKey("email") && requestMap.containsKey("password")) {
+            return true;
+        }
+        return false;
+
+
+    }
+
+
 }
