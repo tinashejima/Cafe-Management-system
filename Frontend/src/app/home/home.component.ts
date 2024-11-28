8d5e1a9bee5f567a8b7b12e4bd85c02453c838b8
tinashejima <jimatinashe@gmail.com>
2024-11-28 12:45:38 +0200

    add frontend folder

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { SignupComponent } from '../signup/signup.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,15 +6,9 @@ import { SignupComponent } from '../signup/signup.component';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private dialog:MatDialog) { }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-
-  handleSignupAction(){
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.width = "550px";
-    this.dialog.open(SignupComponent, dialogConfig);
   }
 
 }
