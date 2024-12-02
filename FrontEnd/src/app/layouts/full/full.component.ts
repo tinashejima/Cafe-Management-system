62492314a8d972f73cf98d4f489ee07583d6626f
tinashejima <jimatinashe@gmail.com>
2024-12-02 17:33:52 +0200

    add components

---
+++ FrontEnd/src/app/layouts/full/full.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-full',
+  imports: [],
+  templateUrl: './full.component.html',
+  styleUrl: './full.component.css'
+})
+export class FullComponent {
+
+}
---
+++ FrontEnd/src/app/material-component/dialog/view-bill-products/view-bill-products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-bill-products',
+  imports: [],
+  templateUrl: './view-bill-products.component.html',
+  styleUrl: './view-bill-products.component.css'
+})
+export class ViewBillProductsComponent {
+
+}
