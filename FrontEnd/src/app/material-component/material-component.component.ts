129b8bb6002eb8d8d07c73cb69e79160b07236da
tinashejima <jimatinashe@gmail.com>
2024-12-02 21:07:33 +0200

    add components

---
+++ FrontEnd/src/app/material-component/material-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-material-component',
+  imports: [],
+  templateUrl: './material-component.component.html',
+  styleUrl: './material-component.component.css'
+})
+export class MaterialComponentComponent {
+
+}
